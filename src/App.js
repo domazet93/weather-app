c8151144906438a405ea5fb403c129b2041cdae2
Toni Domazet <toni.domaz93@hotmail.com>
2018-10-16 21:23:35 +0100

    install axios package for manage HTTP calls
    create .env file for storing configuration variables
    
    create axios instance - openweathermap API

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
-import React, { Component } from 'react';
-
-
-
+import React, { Component } from "react";
+import axios from "./axios/axios-openweather";
 class App extends Component {
+  componentDidMount() {
+    axios
+      .get("/forecast", {
+        params: { 
+          q: "München",
+          appid: process.env.REACT_APP_API_KEY
+        }
+      })
+      .then(res => console.log(res))
+      .catch(err => console.log(err));
+  }
+
   render() {
-    return (
-      <p>Hello</p>
-    );
+    return <p>Hello</p>;
   }
 }
 
